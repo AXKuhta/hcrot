6b9d0a3955adf6f04c5aef8720db494c8db0266c
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-03-13 23:36:21 +0800

    Let's begin: optimized f32+f32

---
+++ simd/f32.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <assert.h>
+
+typedef float f32;
+
+// SSE2 	2 loops (-O2 -march=core2)
+// AVX 		4 ops	(-O2 -march=sandybridge)
+// AVX2 	2 ops	(-O2 -march=skylake)
+// AVX512F 	1 op 	(-O2 -march=icelake-server)
+#define K_STRIDE 16
+
+static int strides_cleanly(const size_t size) {
+	return size % K_STRIDE == 0;
+}
+
+void f32_add_f32(f32* restrict a, f32* restrict b, const size_t size) {
+	assert(strides_cleanly(size));
+
+	for (size_t i = 0; i < size; i += K_STRIDE) {
+		for (size_t j = 0; j < K_STRIDE; j++) {
+			a[i + j] += b[i + j];
+		}
+	}
+}
