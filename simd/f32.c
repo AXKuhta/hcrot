996b557b6d0c18690296c4ec0189b81305591fdb
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-03-26 20:53:17 +0800

    It turns out you need -ftree-vectorize on gcc 11
    The seconds version of dot_f32_f32() is significantly faster now

--- simd/f32.c
+++ simd/f32.c
@@ -30,23 +30,10 @@ f32 dot_f32_f32(f32* restrict a, f32* restrict b, const size_t size) {
 	assert(strides_cleanly(size));
 
 	size_t elements = size / sizeof(f32);
-	f32 acc = 0.0;
-
-	for (size_t i = 0; i < elements; i += K_STRIDE_F32) {
-		for (size_t j = 0; j < K_STRIDE_F32; j++) {
-			acc += a[i + j] * b[i + j];
-		}
-	}
-
-	return acc;
-}
-
-/*
-f32 dot_f32_f32(f32* restrict a, f32* restrict b, const size_t size) {
 	f32 accs[K_STRIDE_F32] = {0.0};
 	f32 acc = 0.0;
 
-	for (size_t i = 0; i < size; i += K_STRIDE_F32) {
+	for (size_t i = 0; i < elements; i += K_STRIDE_F32) {
 		for (size_t j = 0; j < K_STRIDE_F32; j++) {
 			accs[j] += a[i + j] * b[i + j];
 		}
@@ -57,4 +44,3 @@ f32 dot_f32_f32(f32* restrict a, f32* restrict b, const size_t size) {
 
 	return acc;
 }
-*/
