f9fd8de3b06164618ab86f6946e2cb52497e5b4e
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-03-30 20:07:16 +0800

    Drop index.h

--- include/api.h
+++ include/api.h
@@ -2,6 +2,4 @@
 #include "simd.h"
 #include "tensor_t.h"
 //#include "print.h"
-#include "index.h"
 //#include "ops.h"
-
--- include/tensor_t.h
+++ include/tensor_t.h
@@ -70,3 +70,22 @@ i32_tensor* ones_init_i32_tensor(i32_tensor* tensor);
 i32_tensor* rand_init_i32_tensor(i32_tensor* tensor);
 
 void free_tensor(void* tensor);
+
+// ============================================================================
+// INDEXING
+// ============================================================================
+
+static size_t linear_index(const tensor_shape_t* shape, size_t index_dimensions, size_t index[]) {
+	assert(shape->dimensions == index_dimensions);
+
+	size_t offset = shape->dimensions - index_dimensions;
+	size_t linear_index = 0;
+
+	for (size_t i = 0; i < index_dimensions; i++) {
+		linear_index += index[i] * shape->arr[i + offset].stride;
+	}
+
+	return linear_index;
+}
+
+#define idx(x, ...) *(x->storage + linear_index(&x->shape, Index(__VA_ARGS__)))
