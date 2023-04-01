874fe07226c65743c5fdd58ef0034d7ac4e8a665
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-04-01 22:54:47 +0800

    Add min/max/mean to demo

--- demo.c
+++ demo.c
@@ -6,13 +6,13 @@
 #include "api.h"
 
 void tensor_initialization() {
-	tensor_t* a = zeros_tensor(f32, 4, 4);
-	tensor_t* b = ones_tensor(f32, 4, 4);
-	tensor_t* c = rand_tensor(f32, 4, 4);
-
 	printf("===============================================\n");
 	printf("Zeros/ones/random initialization\n");
 	printf("===============================================\n");
+
+	tensor_t* a = zeros_tensor(f32, 4, 4);
+	tensor_t* b = ones_tensor(f32, 4, 4);
+	tensor_t* c = rand_tensor(f32, 4, 4);
 
 	printf("Zeros tensor:\n");
 	print_tensor(a);
@@ -31,13 +31,13 @@ void tensor_initialization() {
 }
 
 void inplace_math() {
-	tensor_t* a = rand_tensor(f32, 4, 4);
-	tensor_t* b = rand_tensor(f32, 4, 4);
-	tensor_t* c = ones_tensor(f32, 4, 4);
-
 	printf("===============================================\n");
 	printf("Inplace math\n");
 	printf("===============================================\n");
+
+	tensor_t* a = rand_tensor(f32, 4, 4);
+	tensor_t* b = rand_tensor(f32, 4, 4);
+	tensor_t* c = ones_tensor(f32, 4, 4);
 
 	printf("Inplace add:\n");
 	print_tensor(a);
@@ -76,7 +76,21 @@ void inplace_math() {
 	printf("\n");
 }
 
+void reduction_ops() {
+	printf("===============================================\n");
+	printf("Reduction operations: min/max/mean\n");
+	printf("===============================================\n");
+	
+	tensor_t* a = rand_tensor(f32, 4, 4);
+	print_tensor(a);
+
+	printf("Min: %.4f\n", tensor_min_f32(a));
+	printf("Max: %.4f\n", tensor_max_f32(a));
+	printf("Mean: %.4f\n", tensor_mean_f32(a));
+}
+
 int main() {
 	tensor_initialization();
 	inplace_math();
+	reduction_ops();
 }
