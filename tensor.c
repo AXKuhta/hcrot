209ce7b1c73d5b89865869b4cc7fd2728a905f28
AXKuhta <11133304+AXKuhta@users.noreply.github.com>
2023-05-14 22:28:58 +0800

    Remove contiguous_storage(), call contiguous_recursive_fn() directly
    Somehow this made transposition faster:
    
    bench_transpose_square: 285 us/iter
    bench_transpose_nonsquare: 281 us/iter

--- tensor.c
+++ tensor.c
@@ -45,31 +45,30 @@ static tensor_t* clone_tensor_struct(tensor_t* tensor) {
 	return clone;
 }
 
-static void contiguous_recursive_fn(void* dst, tensor_t* tensor, size_t base, size_t dim) {
+static void* contiguous_recursive_fn(tensor_t* tensor, size_t base, size_t dim) {
 	size_t element_size = tensor->element_size;
 	size_t dimensions = tensor->dimensions;
 	size_t stride = tensor->shape[dim].stride;
 	size_t size = tensor->shape[dim].size;
 
 	static size_t src_idx;
+	static u8* dst;
 
-	if (dim == 0)
+	if (dim == 0) {
+		dst = malloc(tensor->element_count * tensor->element_size);
 		src_idx = 0;
+	}
 
 	if (dim == dimensions) {
-		memcpy((u8*)dst + base*element_size, tensor->storage.u8 + src_idx*element_size, element_size);
+		memcpy(dst + base*element_size, tensor->storage.u8 + src_idx*element_size, element_size);
 		src_idx++;
 	} else {
 		for (size_t i = 0; i < size; i++) {
-			contiguous_recursive_fn(dst, tensor, base + i*stride, dim + 1);
+			contiguous_recursive_fn(tensor, base + i*stride, dim + 1);
 		}
 	}
-}
 
-static void* contiguous_storage(tensor_t* tensor) {
-	void* memory = malloc(tensor->element_count * tensor->element_size);
-	contiguous_recursive_fn(memory, tensor, 0, 0);
-	return memory;
+	return dst;
 }
 
 static void swap(size_t* a, size_t* b) {
@@ -86,7 +85,7 @@ tensor_t* transpose_tensor(tensor_t* tensor) {
 	transposed->shape[0].stride = 1;
 	transposed->shape[1].stride = rows;
 
-	transposed->storage.memory = contiguous_storage(transposed);
+	transposed->storage.memory = contiguous_recursive_fn(transposed, 0, 0);;
 
 	swap(&transposed->shape[0].stride, &transposed->shape[1].stride);
 	swap(&transposed->shape[0].size, &transposed->shape[1].size);
